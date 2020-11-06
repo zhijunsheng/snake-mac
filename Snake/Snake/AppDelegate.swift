fff3504e17b93719f7c8ccf735bcaf46cc8d8fcd
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-06 06:32:00 -0500

    created snake xcode proj

---
+++ Snake/Snake/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Snake
+//
+//  Created by Golden Thumb on 2020-11-06.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Snake/Snake/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Snake
+//
+//  Created by Golden Thumb on 2020-11-06.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
