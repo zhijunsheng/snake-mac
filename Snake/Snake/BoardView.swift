ce42ecb43e39ddfeaaaeff10d2039f13559ff514
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-06 09:45:04 -0500

    added vertical lines to the grid of game board

--- Snake/Snake/BoardView.swift
+++ Snake/Snake/BoardView.swift
@@ -15,16 +15,26 @@ class BoardView: NSView {
         #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1).setFill()
         bounds.fill()
         
+        let cols: Int = 22
+        let rows: Int = 11
+        let gridX: CGFloat = 23
+        let gridY: CGFloat = 13
+        let cellSide: CGFloat = 19
+        
         let path = NSBezierPath()
         
-        for i in 0..<10 {
-            path.move(to: NSPoint(x: 40, y: 20 + i * 17))
-            path.line(to: NSPoint(x: 400, y: 20 + i * 17))
+        for i in 0..<rows {
+            path.move(to: NSPoint(x: gridX, y: gridY + CGFloat(i) * cellSide))
+            path.line(to: NSPoint(x: gridX + CGFloat(cols - 1) * cellSide, y: gridY + CGFloat(i) * cellSide))
+        }
+        
+        for i in 0..<cols {
+            path.move(to: NSPoint(x: gridX + CGFloat(i) * cellSide, y: gridY))
+            path.line(to: NSPoint(x: gridX + CGFloat(i) * cellSide, y: gridY + CGFloat(rows - 1) * cellSide))
         }
         
         #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0).setStroke()
         path.stroke()
-        
     }
     
 }
